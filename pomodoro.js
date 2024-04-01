d82c1c3adaf21f461ed0bfc932f9f74ec81652f4
Matheus Eduardo Beloli <beloli2005@gmail.com>
2024-04-01 11:38:29 -0300

    finishing

--- pomodoro.js
+++ pomodoro.js
@@ -6,7 +6,7 @@ function getExercise() {
     "https://api.api-ninjas.com/v1/exercises?type=stretching&offset=" + offset,
     {
       method: "GET",
-      headers: { "X-Api-Key": "My api" },
+      headers: { "X-Api-Key": "jPdhdlawZpPuFrvft5TksAfuSanhftgzqWd6smRi" },
       contentType: "application/json",
     }
   )
@@ -17,78 +17,85 @@ function getExercise() {
     })
     .catch((error) => console.log(error));
 }
-et timer;
-        let oneSecond = 1000;
-        let remainingTime = { minutes: 25, seconds: 0 };
+ let timer;
+ let oneSecond = 10;
+ let remainingTime = { minutes: 25, seconds: 0 };
 
-        function startStudyTimer() {
-            pomodoro();
-            document.getElementById("pauseButton").disabled = false;
-            document.getElementById("startStudyButton").disabled = true;
-            document.getElementById("startRestButton").disabled = true;
+ function startStudyTimer() {
+   pomodoro();
+   document.getElementById("pauseButton").disabled = false;
+   document.getElementById("startStudyButton").disabled = true;
+   document.getElementById("startRestButton").disabled = true;
 
-            timer = setInterval(function () {
-                if (remainingTime.seconds === 0 && remainingTime.minutes === 0) {
-                    clearInterval(timer);
-                    document.getElementById("startRestButton").disabled = false;
-                    alert("Fim do tempo de estudo!");
-                } else if (remainingTime.seconds === 0) {
-                    remainingTime.seconds = 59;
-                    remainingTime.minutes--;
-                } else {
-                    remainingTime.seconds--;
-                }
-                updateTimerDisplay();
-            }, oneSecond);
-        }
+   timer = setInterval(function () {
+     if (remainingTime.seconds === 0 && remainingTime.minutes === 0) {
+       clearInterval(timer);
+       document.getElementById("startRestButton").disabled = false;
+       alert("Fim do tempo de estudo!");
+     } else if (remainingTime.seconds === 0) {
+       remainingTime.seconds = 59;
+       remainingTime.minutes--;
+     } else {
+       remainingTime.seconds--;
+     }
+     updateTimerDisplay();
+   }, oneSecond);
+ }
 
-        function startRestTimer() {
-            alongar();
-            document.getElementById("pauseButton").disabled = false;
-            document.getElementById("startRestButton").disabled = true;
-            document.getElementById("startStudyButton").disabled = true;
+ function startRestTimer() {
+   alongar();
+   remainingTime = { minutes: 5, seconds: 0 }; // Define o tempo de descanso para 5 minutos (300 segundos)
+   document.getElementById("pauseButton").disabled = false;
+   document.getElementById("startRestButton").disabled = true;
+   document.getElementById("startStudyButton").disabled = true;
 
-            timer = setInterval(function () {
-                if (remainingTime.seconds === 0 && remainingTime.minutes === 0) {
-                    clearInterval(timer);
-                    document.getElementById("startStudyButton").disabled = false;
-                    alert("Fim do tempo de descanso!");
-                } else if (remainingTime.seconds === 0) {
-                    remainingTime.seconds = 59;
-                    remainingTime.minutes--;
-                } else {
-                    remainingTime.seconds--;
-                }
-                updateTimerDisplay();
-            }, oneSecond);
-        }
+   timer = setInterval(function () {
+     if (remainingTime.seconds === 0 && remainingTime.minutes === 0) {
+       clearInterval(timer);
+       document.getElementById("startStudyButton").disabled = false;
+       alert("Fim do tempo de descanso!");
+     } else if (remainingTime.seconds === 0) {
+       remainingTime.seconds = 59;
+       remainingTime.minutes--;
+     } else {
+       remainingTime.seconds--;
+     }
+     updateTimerDisplay();
+   }, oneSecond);
+ }
 
-        function pauseTimer() {
-            clearInterval(timer);
-            document.getElementById("startStudyButton").disabled = false;
-            document.getElementById("startRestButton").disabled = false;
-            document.getElementById("pauseButton").disabled = true;
-        }
+ function pauseTimer() {
+   clearInterval(timer);
+   document.getElementById("startStudyButton").disabled = false;
+   document.getElementById("startRestButton").disabled = false;
+   document.getElementById("pauseButton").disabled = true;
+ }
 
-        function updateTimerDisplay() {
-            const minutes = remainingTime.minutes;
-            const seconds = remainingTime.seconds;
-            const timerDisplay = document.getElementById("timerDisplay");
-            timerDisplay.textContent = `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
-        }
+ function updateTimerDisplay() {
+   const minutes = remainingTime.minutes;
+   const seconds = remainingTime.seconds;
+   const timerDisplay = document.getElementById("timerDisplay");
+   timerDisplay.textContent = `${minutes < 10 ? `0${minutes}` : minutes}:${
+     seconds < 10 ? `0${seconds}` : seconds
+   }`;
+ }
 
-        function pomodoro() {
-            let inner = document.getElementById("inner");
-            inner.textContent = "Let´s study";
-        }
+ function pomodoro() {
+   let inner = document.getElementById("inner");
+   inner.textContent = "Let´s study";
+ }
 
-        function alongar() {
-            let inner = document.getElementById("inner");
-            let describe = document.getElementById("descricao");
-            inner.textContent = `alongamento`;
-            describe.textContent = `Instruções de alongamento...`;
-        }
+ function alongar() {
+   let inner = document.getElementById("inner");
+   let describe = document.getElementById("descricao");
+   inner.textContent = `alongamento: ${exercises[ex].name}`;
+   describe.textContent = `Instruções: ${exercises[i].ins}`;
+ }
 
-        document.getElementById("startStudyButton").addEventListener("click", startStudyTimer);
-        document.getElementById("startRestButton").addEventListener("click", startRestTimer);
-        document.getElementById("pauseButton").addEventListener("click", pauseTimer);
+ document
+   .getElementById("startStudyButton")
+   .addEventListener("click", startStudyTimer);
+ document
+   .getElementById("startRestButton")
+   .addEventListener("click", startRestTimer);
+ document.getElementById("pauseButton").addEventListener("click", pauseTimer);
